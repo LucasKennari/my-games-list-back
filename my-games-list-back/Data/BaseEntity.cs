b61a94bd213fef506703fe1e5388c566ac32e8ca
Henrique Farias <henriqueti311298@gmail.com>
2024-06-26 22:50:14 -0300

    Exclusão da pasta controller Criação da BaseEntity e UserEntity

---
+++ my-games-list-back/Data/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace my_games_list_back.Data
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; private set; }
+
+        public bool IsActive { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        public DateTime UpdatedAt { get; private set; }
+
+        protected BaseEntity() { }
+
+        public BaseEntity(Guid id, bool isActive, DateTime createdAt, DateTime updatedAt)
+        {
+            Id = id;
+            IsActive = isActive;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+        }
+    }
+}
---
+++ my-games-list-back/Features/Users/UserEntity.cs
@@ -0,0 +1,24 @@
+﻿using my_games_list_back.Data;
+
+namespace my_games_list_back.Features.Users
+{
+    public class UserEntity : BaseEntity
+    {
+        public string Name { get; private set; }
+        public string Nickname { get; private set; }
+        public DateTime Birthday { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        private UserEntity() { }
+
+        public UserEntity(string name, string nickname, DateTime birthday, string email, string password)
+        {
+            Name = name;
+            Nickname = nickname;
+            Birthday = birthday;
+            Email = email;
+            Password = password;
+        }
+    }
+}
